378ec0eb164bacaeca54a7375fb1b4939d9701c4
jay16udani <jay16udani@gmail.com>
2017-07-21 01:16:29 -0400

    PA1 complete

--- programming assignment_1/connected_components/connected_components.cpp
+++ programming assignment_1/connected_components/connected_components.cpp
@@ -4,21 +4,51 @@
 using std::vector;
 using std::pair;
 
-int number_of_components(vector<vector<int> > &adj) {
-  int res = 0;
-  //write your code here
-  return res;
+
+void reach(vector<pair<bool,vector<int> > > &adj, int x)
+{
+  //DFS implementation
+  adj[x].first = 1;
+  for(int i=0; i<(int)adj[x].second.size(); i++)
+  {
+	  if(adj[(adj[x].second)[i]].first == 0)
+	  {
+		  reach(adj,((adj[x].second)[i]));
+	  }
+  }
 }
 
 int main() {
   size_t n, m;
+  int counter = 0;
   std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
+  vector<pair<bool,vector<int> > > adj(n);
+  
+  for(int i=0; i<(int)n; i++)
+	  adj[i].first = 0;
+  
   for (size_t i = 0; i < m; i++) {
     int x, y;
     std::cin >> x >> y;
-    adj[x - 1].push_back(y - 1);
-    adj[y - 1].push_back(x - 1);
+    adj[x - 1].second.push_back(y - 1);
+    adj[y - 1].second.push_back(x - 1);
   }
-  std::cout << number_of_components(adj);
+  /*std::cout << "\nGraph is:\n";
+  for(int i=0; i<(int)adj.size(); i++)
+  {
+	  std::cout << i+1 << " ";
+	  for(int j=0; j<(int)adj[i].second.size(); j++)
+		  std::cout << ((adj[i].second)[j])+1 << " ";
+	  std::cout << "\n";
+  }*/
+  for(int i=0; i<(int)n; i++)
+  {
+	  if(adj[i].first == 0)
+	  {
+		  reach(adj,i);
+		  counter++;
+	  }
+  }
+  std::cout << counter << std::endl;
+  return 0;
 }
--- programming assignment_1/reachability/reachability.cpp
+++ programming assignment_1/reachability/reachability.cpp
@@ -5,21 +5,53 @@ using std::vector;
 using std::pair;
 
 
-int reach(vector<vector<int> > &adj, int x, int y) {
-  //write your code here
-  return 0;
+int reach(vector<pair<bool,vector<int> > > &adj, int x, int y) {
+  //DFS implementation
+  if(x == y )
+	  return 1;
+  else
+  {
+	  adj[x].first = 1;
+	  for(int i=0; i<(int)adj[x].second.size(); i++)
+	  {
+		  if(adj[(adj[x].second)[i]].first == 0)
+		  {
+			  if(((adj[x].second)[i]) == y)
+				  return 1;
+			  else
+			  {
+				  int c = reach(adj,((adj[x].second)[i]),y);
+				  if(c==1)
+					  return 1;
+			  }
+		  }
+	  }
+	  return 0;
+  }
 }
 
 int main() {
   size_t n, m;
   std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
+  vector<pair<bool,vector<int> > > adj(n);
+  
+  for(int i=0; i<(int)n; i++)
+	  adj[i].first = 0;
+  
   for (size_t i = 0; i < m; i++) {
     int x, y;
     std::cin >> x >> y;
-    adj[x - 1].push_back(y - 1);
-    adj[y - 1].push_back(x - 1);
+    adj[x - 1].second.push_back(y - 1);
+    adj[y - 1].second.push_back(x - 1);
   }
+  /*std::cout << "\nGraph is:\n";
+  for(int i=0; i<(int)adj.size(); i++)
+  {
+	  std::cout << i+1 << " ";
+	  for(int j=0; j<(int)adj[i].second.size(); j++)
+		  std::cout << ((adj[i].second)[j])+1 << " ";
+	  std::cout << "\n";
+  }*/
   int x, y;
   std::cin >> x >> y;
   std::cout << reach(adj, x - 1, y - 1);
