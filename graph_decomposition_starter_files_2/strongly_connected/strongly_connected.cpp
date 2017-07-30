431d1543bd22e23fd9572b22313baaea2670cddc
jay16udani <jay16udani@gmail.com>
2017-07-30 02:54:39 -0400

    week 2 done

--- graph_decomposition_starter_files_2/strongly_connected/strongly_connected.cpp
+++ graph_decomposition_starter_files_2/strongly_connected/strongly_connected.cpp
@@ -1,24 +1,91 @@
-#include <algorithm>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-using std::vector;
-using std::pair;
+using namespace std;
+int clock_counter = 0;
 
-int number_of_strongly_connected_components(vector<vector<int> > adj) {
-  int result = 0;
-  //write your code here
-  return result;
+class Vertices
+{
+	public:
+		bool visited;
+		vector <int> paths;
+		
+	Vertices()
+	{
+		visited = false;
+		paths.resize(0);
+	}	
+};
+
+void explore(Vertices myvertices[], int x)
+{
+	myvertices[x].visited = true;
+	for(int i=0; i<(int)myvertices[x].paths.size(); i++)
+	{
+	  if(myvertices[(myvertices[x].paths)[i]].visited == false)
+	  {
+		  explore(myvertices,((myvertices[x].paths)[i]));
+	  }
+	}
 }
 
-int main() {
-  size_t n, m;
-  std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  for (size_t i = 0; i < m; i++) {
+//RUN DFS on reverse graph
+void dfs_reverse_graph(Vertices reverse_graph[], int x, vector <pair<int,int> > &post_vertex)
+{
+	reverse_graph[x].visited = true;
+	clock_counter++;
+	for(int i=0; i<(int)reverse_graph[x].paths.size(); i++)
+	{
+	  if(reverse_graph[(reverse_graph[x].paths)[i]].visited == false)
+	  {
+		  dfs_reverse_graph(reverse_graph,((reverse_graph[x].paths)[i]), post_vertex);
+	  }
+	}
+	post_vertex.push_back(make_pair(clock_counter,x));
+	clock_counter++;
+}
+
+int main()
+{
+  int n, m,counter = 0;
+  vector <pair<int,int> > post_vertex; //vector to store pairs of postvisit numbers and vertex index
+  cin >> n >> m;
+  
+  Vertices myvertices[n];
+  Vertices reverse_graph[n];
+  
+  for (int i = 0; i<m; i++)
+  {
     int x, y;
-    std::cin >> x >> y;
-    adj[x - 1].push_back(y - 1);
+    cin >> x >> y;
+    myvertices[x - 1].paths.push_back(y - 1);
+	reverse_graph[y - 1].paths.push_back(x - 1);
   }
-  std::cout << number_of_strongly_connected_components(adj);
+  
+  //running DFS on reverse graph for postvisit numbers
+  for(int i=0; i<(int)n; i++)
+  {
+	  if(reverse_graph[i].visited == false)
+	  {
+		  dfs_reverse_graph(reverse_graph,i,post_vertex);
+	  }
+  }
+  
+  //sort the pair vector in descending order according to post visit numbers
+  std::sort(post_vertex.begin(), post_vertex.end(), [](const std::pair<int,int> &left, const std::pair<int,int> &right) {
+    return left.first > right.first;
+});  
+  
+  for(int i=0; i<(int)post_vertex.size(); i++)
+  {
+	  if(myvertices[post_vertex[i].second].visited == false)
+	  {
+		  counter++;
+		  explore(myvertices,post_vertex[i].second);
+	  }
+  }
+  
+  cout << counter;
+  return 0;
 }
