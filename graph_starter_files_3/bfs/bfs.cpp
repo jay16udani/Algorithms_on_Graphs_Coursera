6c6819e563d5cfacd3936dc23d5580c31af0e31c
jay16udani <jay16udani@gmail.com>
2017-08-05 20:47:44 -0400

    week 3 done

--- graph_starter_files_3/bfs/bfs.cpp
+++ graph_starter_files_3/bfs/bfs.cpp
@@ -4,22 +4,56 @@
 
 using std::vector;
 using std::queue;
+using std::pair;
 
-int distance(vector<vector<int> > &adj, int s, int t) {
+int distance(vector<pair <int,vector<int> > > &adj, int s, int t) {
   //write your code here
-  return -1;
+  int c = 0;
+  queue <int> vertices;
+  adj[s].first = 0;
+  vertices.push(s);
+  
+  while(!vertices.empty())
+  {
+	  int temp = vertices.front();
+	  vertices.pop();
+	  for(int i=0; i<(int)adj[temp].second.size(); i++)
+	  {
+		  if(adj[((adj[temp].second)[i])].first == -1)
+		  {
+			  vertices.push(((adj[temp].second)[i]));
+			  adj[((adj[temp].second)[i])].first = (adj[temp].first) + 1;
+			  if(((adj[temp].second)[i]) == t)
+			  {
+				  c = 1;
+				  break;
+			  }
+		  }
+	  }
+	  if(c == 1)
+		  break;
+  }
+  
+  if(c == 1)
+	  return adj[t].first;
+  else
+	return -1;
 }
 
 int main() {
   int n, m;
   std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
+  vector<pair <int, vector<int> > > adj(n);
   for (int i = 0; i < m; i++) {
     int x, y;
     std::cin >> x >> y;
-    adj[x - 1].push_back(y - 1);
-    adj[y - 1].push_back(x - 1);
+    adj[x - 1].second.push_back(y - 1);
+    adj[y - 1].second.push_back(x - 1);
   }
+  //setting all the distances initially to infinity(-1)
+  for(int i=0; i<n; i++)
+	  adj[i].first = -1;
+  
   int s, t;
   std::cin >> s >> t;
   s--, t--;
--- graph_starter_files_3/bipartite/bipartite.cpp
+++ graph_starter_files_3/bipartite/bipartite.cpp
@@ -4,21 +4,61 @@
 
 using std::vector;
 using std::queue;
+using std::pair;
 
-int bipartite(vector<vector<int> > &adj) {
+int distance(vector<pair <pair <int,bool>,vector<int> > > &adj) {
   //write your code here
-  return -1;
+  int c = 0;
+  queue <int> vertices;
+  adj[0].first.second = false;
+  vertices.push(0);
+  
+  while(!vertices.empty())
+  {
+	  int temp = vertices.front();
+	  vertices.pop();
+	  for(int i=0; i<(int)adj[temp].second.size(); i++)
+	  {
+		  if(adj[((adj[temp].second)[i])].first.first == -1) //when you discover a previously undiscovered edge
+		  {
+			  vertices.push(((adj[temp].second)[i]));
+			  adj[((adj[temp].second)[i])].first.first = 1;
+			  adj[((adj[temp].second)[i])].first.second = !adj[temp].first.second;
+		  }
+		  
+		  else if(adj[((adj[temp].second)[i])].first.first == 1)
+		  {
+			  if(adj[((adj[temp].second)[i])].first.second != (!adj[temp].first.second))
+			  {
+				  c = 1;
+				  break;
+			  }
+		  }
+	  }
+	  if(c == 1)
+		  break;
+  }
+  
+  if(c == 1)
+	  return 0;
+  else
+	return 1;
 }
 
 int main() {
   int n, m;
   std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
+  vector<pair <pair<int,bool>, vector<int> > > adj(n);
   for (int i = 0; i < m; i++) {
     int x, y;
     std::cin >> x >> y;
-    adj[x - 1].push_back(y - 1);
-    adj[y - 1].push_back(x - 1);
+    adj[x - 1].second.push_back(y - 1);
+    adj[y - 1].second.push_back(x - 1);
   }
-  std::cout << bipartite(adj);
+  //setting all the vertices to being undiscovered
+  for(int i=0; i<n; i++)
+	  adj[i].first.first = -1;
+  
+  std::cout << distance(adj);
+  return 0;
 }
