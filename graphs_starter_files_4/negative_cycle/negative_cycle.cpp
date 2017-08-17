2543c802e7adf21a3bfd9832d27b4b8882547aef
jay16udani <jay16udani@gmail.com>
2017-08-17 03:28:17 -0400

    week 4 done

--- graphs_starter_files_4/negative_cycle/negative_cycle.cpp
+++ graphs_starter_files_4/negative_cycle/negative_cycle.cpp
@@ -1,23 +1,142 @@
 #include <iostream>
+#include <limits>
+#include <unordered_map>
 #include <vector>
 
 using std::vector;
+using std::unordered_map;
 
-int negative_cycle(vector<vector<int> > &adj, vector<vector<int> > &cost) {
-  //write your code here
+bool relax( int u, int v, vector< long long > &prev,
+            unordered_map< int, long long > &dist,
+            const vector< vector< int > > &cost )
+{
+  long long new_w = dist[ u ] + cost[ u ][ v ];
+  if ( dist[ v ] > new_w )
+  {
+    dist[ v ] = new_w;
+    prev[ v ] = u;
+    return true;
+  }
+  return false;
+}
+
+int negative_cycle( vector< vector< int > > &adj,
+                    vector< vector< int > > &cost )
+{
+  std::unordered_map< int, long long > dist;
+  std::vector< long long > prev( adj.size(), -1 );
+  for ( size_t i = 0; i < adj.size(); ++i )
+  {
+    dist[ i ] = std::numeric_limits< int >::max();
+  }
+
+  dist[ 0 ] = 0;
+
+  int V = adj.size();
+  for ( int i = 0; i < V - 1; ++i )
+  {
+    for ( size_t j = 0; j < adj.size(); ++j )
+    {
+      int u = j;
+      const auto &es = adj[ u ];
+      for ( int v : es )
+      {
+        relax( u, v, prev, dist, cost );
+      }
+    }
+  }
+
+  for ( size_t j = 0; j < adj.size(); ++j )
+  {
+    int u = j;
+    const auto &es = adj[ u ];
+    for ( int v : es )
+    {
+      if ( relax( u, v, prev, dist, cost ) )
+        return 1;
+    }
+  }
   return 0;
 }
 
-int main() {
+int main()
+{
   int n, m;
   std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  vector<vector<int> > cost(n, vector<int>());
-  for (int i = 0; i < m; i++) {
+  vector< vector< int > > adj( n, vector< int >() );
+  vector< vector< int > > cost( n, vector< int >( n, -1 ) );
+  for ( int i = 0; i < m; i++ )
+  {
     int x, y, w;
     std::cin >> x >> y >> w;
-    adj[x - 1].push_back(y - 1);
+    adj[ x - 1 ].push_back( y - 1 );
+    cost[ x - 1 ][ y - 1 ] = w;
+  }
+  std::cout << negative_cycle( adj, cost );
+
+  return 0;
+}
+
+/*#include <iostream>
+#include <vector>
+
+using namespace std;
+int global_sum;
+
+int negative_cycle(vector<pair<bool,vector<int> > > &adj, vector<vector<int> > &cost, int node)
+{
+  //using DFS to detect anamoly
+  adj[node].first = true;
+  for(int i=0; i<(int)adj[node].second.size(); i++)
+  {
+	  global_sum += cost[node][i];
+	  if(adj[(adj[node].second)[i]].first == false)
+	  {
+		  global_sum += negative_cycle(adj,cost,(adj[node].second)[i]);
+	  }
+	  else
+		  return global_sum;
+  }
+  return global_sum;
+}
+
+int main() {
+  int n, m,sum = 0;
+  bool quit = false;
+  
+  std::cin >> n >> m;
+  vector<pair<bool,vector<int> > >adj(n);
+  vector<vector<int> > cost(n);
+  
+  for(int i=0; i<n; i++)
+	adj[i].first = false;
+  
+  for (int i = 0; i < m; i++)
+  {
+    int x, y, w;
+    std::cin >> x >> y >> w;
+    adj[x - 1].second.push_back(y - 1);
     cost[x - 1].push_back(w);
   }
-  std::cout << negative_cycle(adj, cost);
+  
+  for(int i=0; i<n; i++)
+  {
+	  if(adj[i].first == false)
+	  {
+		  global_sum = 0;
+		  sum = negative_cycle(adj, cost, i);
+	  }
+	  if(sum < 0)
+	  {
+		  quit = true;
+		  break;		  
+	  }
+  }
+  if(quit == true)
+	  cout << 1 << endl;
+  else
+	  cout << 0 << endl;
+  
+  return 0;
 }
+*/
